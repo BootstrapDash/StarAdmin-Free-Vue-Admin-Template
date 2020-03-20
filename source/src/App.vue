a4ce84d5f3f7f0ac9af6a5fbb6692b9898eb179c
bootstrapdash-team <git@bootstrapdash.com>
2020-03-20 11:26:09 +0530

    Updated the outdated packages

--- source/src/App.vue
+++ source/src/App.vue
@@ -23,7 +23,7 @@ import AppHeader from '../src/components/partials/AppHeader'
 import AppSidebar from '../src/components/partials/AppSidebar'
 import AppFooter from '../src/components/partials/AppFooter'
 export default{
-  name: 'main',
+  name: 'app',
   components: {
     AppHeader,
     AppSidebar,
