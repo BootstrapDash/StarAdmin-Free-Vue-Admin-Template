1ccaf1e92981be3d8cbe1bc26947380913f54b01
bootstrapdash-team <git@bootstrapdash.com>
2020-11-12 20:43:07 +0530

    copyright text edit

--- demo_1/src/components/partials/AppFooter.vue
+++ demo_1/src/components/partials/AppFooter.vue
@@ -4,7 +4,7 @@
     <footer class="footer">
       <div class="container-fluid clearfix">
         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2020</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.bootstrapdash.com/vue-admin-templates/" target="_blank">Vue template</a> made by BootstrapDash.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/vue-admin-templates/" target="_blank">  vue admin templates </a> from BootstrapDash.com</span>
       </div>
     </footer>
   </section>
